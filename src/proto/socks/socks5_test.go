757be99867d9eb15b389e8b372848d023941bbd6
KujouRinka <kujourinka@gmail.com>
2021-08-01 13:21:26 +0800

    test: rewrite test for sock5 proto

--- src/proto/socks/socks5_test.go
+++ src/proto/socks/socks5_test.go
@@ -1,32 +1,42 @@
 package socks
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
 
 func TestAddrParse(t *testing.T) {
-	pack1 := Socks{
-		Ver:     0,
-		Cmd:     0,
-		AdrType: 0x01,
-		Adr:     []byte{0x7f, 0x00, 0x00, 0x01},	// 127.0.0.1
-		Port:    []byte{0x09, 0x1d},	// 2333
-	}
-	if pack1.String() == "127.0.0.1:2333" {
-		t.Log("sock parse IPv4 successfully")
+	pack1 := Req{
+		0x05, 0x01, 0x00, 0x01, 0x7f,
+		0x00, 0x00, 0x01, 0x09, 0x1d,
+	} // 127.0.0.1:2333
+	if pack1.AdrLen() == net.IPv4len {
+		t.Log("sock5 parse IPv4 len successfully")
 	} else {
-		t.Errorf("sock parse IPv4 error, except \"127.0.0.1:1080\" but given \"%s\"",
-			pack1.String())
+		t.Errorf("sock5 parse IPv4 len error, expect \"%d\" but given \"%d\"",
+			net.IPv4len, pack1.AdrLen())
 	}
-	pack2 := Socks{
-		Ver:     0,
-		Cmd:     0,
-		AdrType: 0x03,
-		Adr:     []byte("google.com"),
-		Port:    []byte{0x01, 0xbb},	// 443
-	}
-	if pack2.String() == "google.com:443" {
-		t.Log("sock parse domain successfully")
+	if pack1.AdrPort() == "127.0.0.1:2333" {
+		t.Log("socks5 parse IPv4 port successfully")
 	} else {
-		t.Errorf("sock parse domain error, except \"google.com:443\" but given \"%s\"",
-			pack2.String())
+		t.Errorf("socks5 parse IPv4 port error, expect \"127.0.0.1:2333\" but given \"%s\"",
+			pack1.AdrPort())
+	}
+
+	pack2 := Req{
+		0x05, 0x01, 0x00, 0x03,
+		0x01, byte('g'), 0x00, 0x50,
+	} //g:80
+	if pack2.AdrLen() == 1 {
+		t.Log("sock5 parse domain len successfully")
+	} else {
+		t.Errorf("sock5 parse domain len error, expect \"1\" but given \"%d\"",
+			pack2.AdrLen())
+	}
+	if pack2.AdrPort() == "g:80" {
+		t.Log("socks5 parse domain successfully")
+	} else {
+		t.Errorf("socks5 parse domain error, expect \"g:80\" but given \"%s\"",
+			pack2.AdrPort())
 	}
 }
